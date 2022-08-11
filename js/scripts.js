fc35d664630358b5adb93f7c1c013d19d10e169f
Gary <gary.tom@mail.mcgill.ca>
2022-08-10 23:52:35 -0700

    Added server

--- js/scripts.js
+++ js/scripts.js
@@ -234,6 +234,30 @@ $(document).ready(function () {
         }
     });
 
+    /********************** Meal **********************/
+    $('#meal-form').on('submit', function (e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your food preferences.'));
+
+        $.post('https://script.google.com/macros/s/AKfycbxKb0JwL4JnTGojuoKS01KzkGn-oeW62LTHL8Mgi_8iInlGugKOst79U5A8A45omhxq/exec', data)
+            .done(function (data) {
+                console.log(data);
+                if (data.result === "error") {
+                    $('#alert-wrapper').html(alert_markup('danger', data.message));
+                } else {
+                    $('#alert-wrapper').html('');
+                    $('#rsvp-modal').modal('show');
+                }
+            })
+            .fail(function (data) {
+                console.log(data);
+                $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+            });
+        }
+    });
+
 });
 
 /********************** Extras **********************/
